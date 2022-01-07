175465f6c37306870dac7e952f8d064adbad5b2c
IcookTacos <zeidlitzerik@gmail.com>
2022-01-07 23:58:33 +0100

    Refactored server from http -> portforward

--- server/server.go
+++ server/server.go
@@ -1,17 +1,56 @@
-package server
+package main
 
 import (
-	"fmt"
-	"net/http"
+	"flag"
+	"io"
+	"log"
+	"net"
+	"os"
 )
 
-const PORT = 8080
+var PORT string
 
-func InitializeServer() {
-	http.HandleFunc("/", Send)
-	http.ListenAndServe(":8080", nil)
+func init() {
+	flag.StringVar(&PORT, "p", os.Getenv("PORT"), "The default port to listen on")
+	flag.Parse()
+
+	if PORT == "" {
+		PORT = "8080"
+	}
+
 }
 
-func Send(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "text %s", request.URL.Path[1:])
+func main() {
+	log.Printf("listening on port %v", PORT)
+	listener, err := net.Listen("tcp", ":"+PORT)
+	if err != nil {
+		log.Fatal("Cannot listen on port")
+		log.Fatal("Error: %s", err)
+		return
+	}
+	defer listener.Close()
+
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			log.Fatal("Could not accept connection")
+			log.Fatal("Error: %s", err)
+			return
+		}
+		go handleConnection(connection)
+	}
+}
+
+func handleConnection(connection net.Conn) {
+	defer connection.Close()
+	log.Printf("Connection accepted %v", connection.RemoteAddr())
+	_, err := io.Copy(connection, connection)
+	if err == io.EOF {
+		log.Printf("Recieved EOF. Client dissconnected")
+		return
+	} else if err != nil {
+		log.Fatal("copy error")
+		log.Fatal("Error: %s", err)
+	}
+
 }
